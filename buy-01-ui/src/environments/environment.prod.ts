982afc88a3b234cd4c7af6f8ec4a77e439298060
oafilali <medafilali@gmail.com>
2026-01-08 14:51:06 +0200

    Force rebuild: Add comment to environment.prod.ts to bypass Docker cache

--- buy-01-ui/src/environments/environment.prod.ts
+++ buy-01-ui/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 // src/environments/environment.prod.ts
-// This is the PRODUCTION file for Docker builds
-export const environment = {
+// This is the PRODUCTION file for Docker builds// Updated: 2026-01-08 - New infrastructure IP addressesexport const environment = {
   production: true,
   apiUrl: 'http://13.61.234.232:8080/api',
   apiGatewayUrl: 'http://13.61.234.232:8080',
