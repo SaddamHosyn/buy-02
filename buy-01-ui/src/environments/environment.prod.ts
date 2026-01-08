339b6c332231763839a216712277f411224d1abf
oafilali <medafilali@gmail.com>
2026-01-08 15:02:53 +0200

    Force rebuild: Add buildTimestamp to invalidate Docker cache

--- buy-01-ui/src/environments/environment.prod.ts
+++ buy-01-ui/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 // src/environments/environment.prod.ts
 // This is the PRODUCTION file for Docker builds
-// Updated: 2026-01-08 - New infrastructure IP addresses
 export const environment = {
   production: true,
   apiUrl: 'http://13.61.234.232:8080/api',
@@ -9,5 +8,6 @@ export const environment = {
   usersUrl: 'http://13.61.234.232:8080/api/users',
   productsUrl: 'http://13.61.234.232:8080/api/products',
   mediaUrl: 'http://13.61.234.232:8080/api/media',
-  enableDebugLogging: false
+  enableDebugLogging: false,
+  buildTimestamp: '2026-01-08T13:00:00Z'
 };
